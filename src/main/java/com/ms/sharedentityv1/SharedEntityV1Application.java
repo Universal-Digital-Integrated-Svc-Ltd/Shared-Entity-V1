6db266aa8984bfe0d2a577290be8baa6f14046dc
Jitender Yadav <jitender1137@gmail.com>
2023-12-10 13:49:18 +0530

    Inital commit

---
+++ src/main/java/com/ms/sharedentityv1/SharedEntityV1Application.java
@@ -0,0 +1,13 @@
+package com.ms.sharedentityv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedEntityV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedEntityV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/sharedentityv1/SharedEntityV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.sharedentityv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedEntityV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
