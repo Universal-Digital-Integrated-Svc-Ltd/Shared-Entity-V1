ba766a36c899576122ebb5251c79552112756413
Jitender Yadav <jitender1137@gmail.com>
2023-12-21 20:02:16 +0530

    updated pom.xml

--- src/main/java/com/ms/sharedentityv1/SharedEntityV1Application.java
+++ src/main/java/com/ms/sharedentityv1/SharedEntityV1Application.java
@@ -1,9 +1,17 @@
 package com.ms.sharedentityv1;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@NoArgsConstructor
+@Getter
+@Setter
+@EnableJpaRepositories
 public class SharedEntityV1Application {
 
 	public static void main(String[] args) {
